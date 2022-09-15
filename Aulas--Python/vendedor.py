18a90f5f8d56bf35cc37a2566711b67c5807d223
Arthur Santos Oliveria <104939137+ArthurSantosOliveira@users.noreply.github.com>
2022-09-15 09:13:47 -0300

    Add files via upload

---
+++ Aulas--Python/vendedor.py
@@ -0,0 +1,22 @@
+cod = float(input("Escolha o código do produto: 1001, 1234, 6548, 0987, 7632: \n"))
+quantidade = int(input("Digite a quantidade do produto que deseja: "))
+
+if(cod == 1001):
+    valor = 5.32 * quantidade
+    print(valor)
+
+if(cod == 1234):
+    valor = 6.45 * quantidade
+    print(valor)
+
+if(cod == 6548):
+    valor = 2.37 * quantidade
+    print(valor)
+    
+if(cod == 987):
+    valor = 5.32 * quantidade
+    print(valor)
+
+if(cod == 7632):
+    valor = 6.45 * quantidade
+    print(valor)
