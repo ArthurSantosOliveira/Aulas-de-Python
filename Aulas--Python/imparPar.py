05d4bbc7263592317111704daa3e10d8976da5a0
Arthur Santos Oliveria <104939137+ArthurSantosOliveira@users.noreply.github.com>
2022-09-15 09:44:44 -0300

    Add files via upload
    impar par com positivo e negativo

---
+++ Aulas--Python/imparPar.py
@@ -0,0 +1,12 @@
+numero = int(input('Digite um inteiro: '))
+
+if numero%2 :
+    print("Ímpar")
+else:
+    print("Par")
+
+if (numero>0):
+    print ("Positivo")
+
+else:
+     print ("Negativo")   
