7e4a329e5e1b257e8bfc8f81a0205d639ae88c14
Arthur Santos Oliveria <104939137+ArthurSantosOliveira@users.noreply.github.com>
2022-09-15 09:34:34 -0300

    Update vendedor.py

--- Aulas--Python/vendedor.py
+++ Aulas--Python/vendedor.py
@@ -3,20 +3,20 @@ quantidade = int(input("Digite a quantidade do produto que deseja: "))
 
 if(cod == 1001):
     valor = 5.32 * quantidade
-    print(valor)
+    print("O preço total é R$",valor)
 
 if(cod == 1234):
     valor = 6.45 * quantidade
-    print(valor)
+    print("O preço total é R$",valor)
 
 if(cod == 6548):
     valor = 2.37 * quantidade
-    print(valor)
+    print("O preço total é R$",valor)
     
 if(cod == 987):
     valor = 5.32 * quantidade
-    print(valor)
+    print("O preço total é R$",valor)
 
 if(cod == 7632):
     valor = 6.45 * quantidade
-    print(valor)
+    print("O preço total é R$",valor)
