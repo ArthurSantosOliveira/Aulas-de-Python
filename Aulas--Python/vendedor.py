b9e0e9c9859cc1980758ea6d7e9ff091342cf2a9
Arthur Santos Oliveria <104939137+ArthurSantosOliveira@users.noreply.github.com>
2022-09-15 09:50:59 -0300

    Update vendedor.py

--- Aulas--Python/vendedor.py
+++ Aulas--Python/vendedor.py
@@ -1,22 +1,22 @@
-cod = float(input("Escolha o código do produto: 1001, 1234, 6548, 0987, 7632: \n"))
+cod = input("Escolha o código do produto: 1001, 1234, 6548, 0987, 7632: \n")
 quantidade = int(input("Digite a quantidade do produto que deseja: "))
 
-if(cod == 1001):
+if(cod == '1001'):
     valor = 5.32 * quantidade
     print("O preço total é R$",valor)
 
-if(cod == 1234):
+if(cod == '1234'):
     valor = 6.45 * quantidade
     print("O preço total é R$",valor)
 
-if(cod == 6548):
+if(cod == '6548'):
     valor = 2.37 * quantidade
     print("O preço total é R$",valor)
     
-if(cod == 987):
+if(cod == '0987'):
     valor = 5.32 * quantidade
     print("O preço total é R$",valor)
 
-if(cod == 7632):
+if(cod == '7632'):
     valor = 6.45 * quantidade
     print("O preço total é R$",valor)
