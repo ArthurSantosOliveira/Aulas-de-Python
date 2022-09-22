cae546a2fe276dd9095b4f57e6e28d91d3ee7091
Arthur Santos Oliveria <104939137+ArthurSantosOliveira@users.noreply.github.com>
2022-09-22 08:27:04 -0300

    Add files via upload

---
+++ Aulas--Python/forIn.py
@@ -0,0 +1,8 @@
+codigos = ['2001','3001','4010'] #veeeectoooor
+
+for codigo in codigos:
+    if codigo[0] == '2':
+        print('manutencao x', codigo)
+    if codigo[0] == '3':
+        print('manutencao y', codigo)
+        
