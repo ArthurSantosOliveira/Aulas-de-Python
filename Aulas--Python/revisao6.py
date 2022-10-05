ec79f14cd953347e1c343e28de15efcde1c88f30
ArthurSantosOliveira <pessoalarthursoliveira@gmail.com>
2022-10-05 19:42:57 -0300

    Update revisao6.py

--- Aulas--Python/revisao6.py
+++ Aulas--Python/revisao6.py
@@ -1,13 +1,20 @@
 contador = 0 
 contador2 = 0
+contadorPorcentagem = 0
 somaIdade = 0
-while(contador < 10):
-    idade=int(input('Digite sua idade:'))
+while(contador < 2):
+    idade=int(input('Digite sua idade: '))
+    altura = float(input('Digite sua altura: '))
     somaIdade = somaIdade + idade
     media1 = somaIdade/10
     contador+=1
     if (idade >=18):
         contador2+=1
-   
+    if ( (idade>18 and idade < 30 and altura > 1.8) ):
+        contadorPorcentagem +=1
+
+porcentagem = contadorPorcentagem/100
+
 print ('A média da idade do grupo é:', media1)
 print('Há', contador2, 'pessoa(s) maior(es) de idade')
+print(porcentagem)
